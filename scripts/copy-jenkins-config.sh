7057d258ca4343a35bae37a345a6d10591dbc38a
Johan Vogelzang <johan.vogelzang@gmail.com>
2015-11-09 15:20:51 +0100

    Added new script to copy Jenkins configuration files to tmp.

---
+++ scripts/copy-jenkins-config.sh
@@ -0,0 +1,6 @@
+# Convenient script for copying all Jenkins configs to one location.
+
+echo Copying Jenkins configuration files to share/tmp 
+
+cp -r docker/volumes/jenkins/var/jenkins_home/config.xml share/tmp/
+find docker/volumes/jenkins/var/jenkins_home/jobs -name 'config.xml' -exec cp --parents \{\} share/tmp \;
